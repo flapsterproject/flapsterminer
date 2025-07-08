b358f9f77e913e0d3b19c41f85817eb4e5522432
flapsterproject <flapster.project@gmail.com>
2025-07-08 17:10:18 +0500

    Add files via upload

---
+++ scripts/c3main.js
@@ -0,0 +1,5 @@
+import "./c3runtime.js";
+import "./plugins/Eponesh_GameScore/c3runtime/main.js";
+import "./plugins/EMI_INDO_LogMessage/c3runtime/main.js";
+import "./plugins/GamePush_Channels/c3runtime/main.js";
+import "./objRefTable.js";
